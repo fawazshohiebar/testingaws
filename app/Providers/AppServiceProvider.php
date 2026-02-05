9b1a968c57102117898ad5bfcdc29eb9d9f6b9ec
fawaz chuhaibar <fawaz.chuhaibar@adnec.ae>
2026-02-05 14:13:44 +0400

    ddasd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if($this->app->environment('production')) {
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
 
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
